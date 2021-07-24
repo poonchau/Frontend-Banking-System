a03845e8a3eb7700006529f2b4380533da209a6e
Poonam <poonamchaursia1998@gmail.com>
2021-07-24 09:42:03 +0530

    first commit

---
+++ src/app/navbar/navbar.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+@Injectable()
+export class NavbarService {
+  visible: boolean;
+
+  public isUserLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+}
